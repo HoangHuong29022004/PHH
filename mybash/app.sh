28cd75b9873c01a843926567c10b561a483c97db
Hoàng Hương <huong.ph@fsofts.com>
2024-12-03 23:17:02 +0700

    save

--- mybash/app.sh
+++ mybash/app.sh
@@ -1,27 +1,36 @@
-#! bin/bash
+#!/bin/bash
+
+# Colors for output
+NORMAL="\\033[0;39m"
+GREEN="\\033[1;32m"
+RED="\\033[1;31m"
+BLUE="\\033[1;34m"
+ORANGE="\\033[1;33m"
 
 # VARIABLES
-FILES_PATH=`ls run/*.sh`
+FILES_PATH=$(ls run/*.sh)
 INDEX=0
 REPLY=""
 declare -a FILES
 
-scan_folder(){
-
-  for EACH_FILE in $FILES_PATH
-  do
+scan_folder() {
+  echo -e "${BLUE}=== Available Scripts ===${NORMAL}"
+  for EACH_FILE in $FILES_PATH; do
     FILES+=("$EACH_FILE")
-    echo "[$INDEX] ${FILES[$INDEX]}"
-    INDEX=$((INDEX+1))
+    echo -e "${GREEN}[$INDEX]${NORMAL} ${FILES[$INDEX]}"
+    INDEX=$((INDEX + 1))
   done
 }
 
-echo "List script:"
-
 scan_folder
 
-read -p "Your choose?: " REPLY
-
-echo "---> Choosed Mode: ${FILES[$REPLY]}..."
-
-bash ${FILES[$REPLY]}
+while true; do
+  read -p "Please enter the number of your choice: " REPLY
+  if [[ $REPLY =~ ^[0-9]+$ ]] && [ $REPLY -ge 0 ] && [ $REPLY -lt $INDEX ]; then
+    echo -e "${ORANGE}---> Selected Script: ${FILES[$REPLY]}...${NORMAL}"
+    bash ${FILES[$REPLY]}
+    break
+  else
+    echo -e "${RED}Invalid choice. Please enter a valid number between 0 and $((INDEX - 1)).${NORMAL}"
+  fi
+done
